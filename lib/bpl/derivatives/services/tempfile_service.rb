9b8024a112447d478ea3ea78b6b3d0de39a8f738
bbarberBPL <bbarber@bpl.org>
2019-03-29 10:15:55 -0400

    Fixed issue with tempfile service

--- lib/bpl/derivatives/services/tempfile_service.rb
+++ lib/bpl/derivatives/services/tempfile_service.rb
@@ -6,16 +6,25 @@ module BPL::Derivatives
       new(object).tempfile(&block)
     end
 
-    attr_reader :source_object
+    attr_reader :source_object, :source_file
 
     def initialize(source_object)
       @source_object = source_object
+      @source_file = nil
+    end
+
+    def source_file
+      if source_object.respond_to? :source_datastream
+        @source_file ||= source_object.source_datastream
+      else
+        @source_file ||= source_object
+      end
     end
 
     def tempfile(&block)
-      if source_object.respond_to? :to_tempfile
-        source_object.send(:to_tempfile, &block)
-      elsif source_object.has_content?
+      if source_file.respond_to? :to_tempfile
+        source_file.send(:to_tempfile, &block)
+      elsif source_file.has_content?
         default_tempfile(&block)
       end
     end
@@ -23,7 +32,7 @@ module BPL::Derivatives
     def default_tempfile(&_block)
       Tempfile.open(filename_for_characterization) do |f|
         f.binmode
-        if source_object.source_file.content.respond_to? :read
+        if source_file.content.respond_to? :read
           f.write(source_file.content.read)
         else
           f.write(source_file.content)
@@ -37,19 +46,19 @@ module BPL::Derivatives
     protected
 
     def filename_for_characterization
-      if source_object.respond_to? :filename_for_characterization
-        source_object.filename_for_characterization
+      if source_file.respond_to? :filename_for_characterization
+        source_file.filename_for_characterization
       else
         default_filename_for_characterization
       end
     end
 
     def default_filename_for_characterization
-      registered_mime_type = MIME::Types[source_object.mime_type].first
-      BPL::Derivatives.base_logger.warn "Unable to find a registered mime type for #{source_file.mime_type.inspect} on #{source_file.uri}" unless registered_mime_type
+      registered_mime_type = MIME::Types[source_file.mime_type].first
+      BPL::Derivatives.base_logger.warn "Unable to find a registered mime type for #{source_file.mime_type.inspect} on #{source_object.uri}" unless registered_mime_type
       extension = registered_mime_type ? ".#{registered_mime_type.extensions.first}" : ''
       version_id = 1 # TODO: fixme
-      m = %r{/([^/]*)$}.match(source_file.uri)
+      m = %r{/([^/]*)$}.match(source_object.uri)
       ["#{m[1]}-#{version_id}", extension.to_s]
     end
   end
